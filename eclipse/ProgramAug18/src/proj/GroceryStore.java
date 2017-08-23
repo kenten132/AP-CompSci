b17d0675e7de84cece09aa9e5a5f7bc9ec84116b
kenten132 <darthcorbin@gmail.com>
2017-08-23 10:08:41 -0400

    .

--- eclipse/ProgramAug18/src/proj/GroceryStore.java
+++ eclipse/ProgramAug18/src/proj/GroceryStore.java
@@ -1,14 +1,17 @@
 /*
  * Grocery Store program. Calculates price of an item based on cost per pound 
- * and the item's weight
+ * and the item's weight multiplied by the sales tax.
  */
-package proj;
 
-import javax.swing.JOptionPane;
 /**
  *
  * @author corbinhiggs
  */
+
+package proj;
+
+import javax.swing.JOptionPane;
+
 public class GroceryStore {
     public static void main(String[] args){
         //variables
@@ -21,17 +24,24 @@ public class GroceryStore {
         poundsOfItems = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter the total pounds needed."));
         
         pricePerPound = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter the price per pound."));
-        
-        salesTax = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter the sales tax rate ('0.__')"));
+        //adds a sales tax function. 
+        salesTax = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter the sales tax rate ('%___')"));
         //calculations.
+        
+        //divides salesTax by 100 to get the true percentage.
+        salesTax /= 100;
         totalCost = poundsOfItems * pricePerPound;
+        
+        //adds sales tax rate to the total cost, making totalCost = salesTax * totalCost (cost of tax) + totalCost (price subtotal)
         totalCost = (totalCost * salesTax) + totalCost;
         
         //outputting results.
-        JOptionPane.showMessageDialog(null, "\nGrocery Store Program\n\n"
+        JOptionPane.showMessageDialog(null, 
+                "\nGrocery Store Program\n\n"
                 + "The toal pounds of item needed: " + poundsOfItems
                 + "\n Price per pound of item: $" + pricePerPound
-                + "\n Sales tax: %" + salesTax
+                /* Adds the sales tax percentage to the final output.*/
+                + "\n Sales tax: %" + salesTax*100
                 + "\nTotal Cost of Item: $" + totalCost);
         System.exit(0);
     }
